c7053e8fb981f1b8c718eccddc60ec5b6e397b97
KietKat <102196529+KietKat@users.noreply.github.com>
2022-06-11 20:47:24 -0700

    Update testDictionary.java

--- testDictionary.java
+++ testDictionary.java
@@ -1,13 +1,19 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class testDictionary {
     public static void main(String[] args) throws Exception {
         File file =new File("words.txt");
         SpellChecker checker= new SpellChecker(file);
-        checker.correctlySpelled("eft");
-        checker.correctlySpelled("bul");
-        checker.correctlySpelled("jlane");
-        checker.correctlySpelled("ope");
-        checker.correctlySpelled("lov");
+        Scanner scan = new Scanner(System.in); 
+        String input;
+        while(true)
+        {
+            System.out.print("Enter a word to check spelling (enter 0 to quit): ");
+            input = scan.nextLine();
+            if(input.charAt(0) == '0') break;
+            checker.correctlySpelled(input);
+        }
+        scan.close();
     }    
 }
